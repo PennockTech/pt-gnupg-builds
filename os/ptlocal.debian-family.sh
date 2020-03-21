303adcf508627e359a9672f4d48d0abbb3973185
Phil Pennock <phil+git@pennock-tech.com>
2020-03-21 03:23:57 -0400

    Keep trusty on life-support just a little longer

--- os/ptlocal.debian-family.sh
+++ os/ptlocal.debian-family.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -eu
 
-cat > /etc/apt/apt.conf.d/70proxy <<'EOPROXY'
+case "$(lsb_release -sc)" in
+trusty)
+  # My creaking proxy is not working for HTTPS with Trusty
+  true
+  ;;
+*)
+  cat > /etc/apt/apt.conf.d/70proxy <<'EOPROXY'
 Acquire::http::Proxy "http://cheddar.lan:3142";
 EOPROXY
+  ;;
+esac
