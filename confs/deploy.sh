0c1dfd6f4b7f78b9acc327a2a21648618477742d
Phil Pennock <phil+git@pennock-tech.com>
2021-03-07 07:04:32 -0500

    Switch aptly server host

--- confs/deploy.sh
+++ confs/deploy.sh
@@ -3,10 +3,10 @@
 REPO_NAME="pt-$MACHINE"
 REPO_DISTRIBUTION="$MACHINE"
 
-SSH_USERHOST='fuji@orchard'
+SSH_USERHOST='pdp@morales'
 REPO_INGEST_DIR="IN-packages/gnupg-$MACHINE"
 REPO_SNAP_PREFIX="${REPO_NAME}-gnupg"
 REPO_KEY='0x8AC8EE39F0C68907'
 REPO_ARCHS='amd64,i386,armel,armhf,arm64'
 REPO_NEED_GPG_AGENT='true'
-REPO_PATH_PREPEND='/opt/gnupg/bin'
+#REPO_PATH_PREPEND='/opt/gnupg/bin'
